8c655a1011485d6a98bd8b4a3a1c2529a5b2304d
Developer <kraevit@gmail.com>
2020-09-04 14:59:31 +0300

    added .ejs

--- express/app.js
+++ express/app.js
@@ -3,8 +3,6 @@ const express = require('express');
 // express app
 const app = express();
 
-// setup view engine
-// npm install ejs
 // register view engine
 app.set('view engine', 'ejs'); // this defaults to 'views' folder
 // app.set('views', 'myviews'); setup a different folder
@@ -12,27 +10,45 @@ app.set('view engine', 'ejs'); // this defaults to 'views' folder
 // listen for requests
 app.listen(3000);
 
-// Routing & HTML
+// ROUTING
+
+// HOME PAGE ROUTE
 app.get('/', (req, res) => {
   // res.send('<p>home page</p>');
   // res.sendFile('./views/index.html', { root: __dirname });
-  ///////////////////////////////////////////////////////////
-  // instead of sending a file
-  // render a view
-  res.render('index');
+
+  // INSTEAD OF SENDING A FILE //
+  
+  // RENDER A VIEW
+  // res.render('index');
+
+  // PASSING DATA from APP TO VIEWS
+  // res.render('index', { title: 'Home'});
+
+  // PASSING ARRAY OF BLOGS
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum'},
+    {title: 'How to defeat browser', snippet: 'Lorem ipsum'}
+  ];
+  // res.render('index', { title: 'Home', blogs: blogs });
+  res.render('index', { title: 'Home', blogs }); // SHORTCUT
 });
 
+// ABOUT PAGE ROUTE
 app.get('/about', (req, res) => {
   // res.send('<p>about page</p>');
   // res.sendFile('./views/about.html', { root: __dirname });
-  res.render('about');
+  res.render('about', { title: 'About' });
 });
 
+// NEW BLOG PAGE ROUTE
 app.get('/blogs/create', (req, res) => {
-  res.render('create');
+  res.render('create', { title: 'Create a new blog' });
 });
 
-// redirects
+// REDIRECTS
+
 // app.get('/about-us', (req, res) => {
 //   res.redirect('/about');
 // });
@@ -44,5 +60,5 @@ app.use((req, res) => {
   res
     .status(404)
     // .sendFile('./views/404.html', { root: __dirname });
-    .render('404');
+    .render('404', { title: '404' });
 });
