57c1c20aead00a6a39400271abf7388c17a03993
Developer <kraevit@gmail.com>
2020-09-07 09:09:24 +0300

    examples using middleware

--- express/app.js
+++ express/app.js
@@ -1,64 +1,75 @@
 const express = require('express');
+const morgan = require('morgan'); // 3rd Party Middleware (Logger)
 
-// express app
+// Init Express App
 const app = express();
 
-// register view engine
-app.set('view engine', 'ejs'); // this defaults to 'views' folder
-// app.set('views', 'myviews'); setup a different folder
+// Set View Engine
+app.set('view engine', 'ejs');  // Default Folder is 'views'
+// app.set('views', 'myviews'); // Setup a Custom Folder 
 
-// listen for requests
+// Listen for Requests on Port 3000
 app.listen(3000);
 
-// ROUTING
+// Middleware
+// Using next() 
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host:' , req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
 
-// HOME PAGE ROUTE
+// Middleware & Static Files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// Basic Routing
+
+// Home Page Route Handler
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  // res.sendFile('./views/index.html', { root: __dirname });
 
-  // INSTEAD OF SENDING A FILE //
+  // Send HTML Code
+  // res.send('<p>home page</p>');
   
-  // RENDER A VIEW
+  // Send HTML File
+  // res.sendFile('./views/index.html', { root: __dirname });
+  
+  // Render Views
   // res.render('index');
 
-  // PASSING DATA from APP TO VIEWS
+  // Pass Data from app.js to 'views'
   // res.render('index', { title: 'Home'});
 
-  // PASSING ARRAY OF BLOGS
+  // Pass Array from app.js to 'views'
   const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum'},
     {title: 'Mario finds stars', snippet: 'Lorem ipsum'},
     {title: 'How to defeat browser', snippet: 'Lorem ipsum'}
   ];
   // res.render('index', { title: 'Home', blogs: blogs });
-  res.render('index', { title: 'Home', blogs }); // SHORTCUT
+  res.render('index', { title: 'Home', blogs }); // Shortcut blogs: blogs to just blog
 });
 
-// ABOUT PAGE ROUTE
+// About Page Route Handler
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  // res.sendFile('./views/about.html', { root: __dirname });
   res.render('about', { title: 'About' });
 });
 
-// NEW BLOG PAGE ROUTE
+// Create Page Route Handler
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
-// REDIRECTS
+// 301 Redirect
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
 
-// app.get('/about-us', (req, res) => {
-//   res.redirect('/about');
-// });
-
-// THIS MUST COME AT THE END BECAUSE WHEN EXPRESS MATCHES A ROUTE IT SEND'S THE REQUEST
-// AND STOPS THERE
-// 404 PAGE
+// 404 - Must Be Always at the Bottom !
 app.use((req, res) => {
   res
     .status(404)
-    // .sendFile('./views/404.html', { root: __dirname });
     .render('404', { title: '404' });
 });
